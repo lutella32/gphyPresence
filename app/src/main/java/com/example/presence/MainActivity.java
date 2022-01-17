303f54bebf0b7600939657d025a454a5685870cc
ltella <lucie.tella@etu.univ-poitiers.fr>
2022-01-17 16:42:56 +0100

    début3

--- app/src/main/java/com/example/presence/MainActivity.java
+++ app/src/main/java/com/example/presence/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.presence;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,4 +14,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
     }
+    public void start(android.view.View v) {
+        Log.d("start","you are in start");
+        Intent lecture = new Intent(this, etudiantControl.class);
+
+        startActivity(lecture);
+    }
+
 }
---
+++ app/src/main/java/com/example/presence/etudiantControl.java
@@ -0,0 +1,14 @@
+package com.example.presence;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class etudiantControl extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_etudiant_control);
+    }
+}
