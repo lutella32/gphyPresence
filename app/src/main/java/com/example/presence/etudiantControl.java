7c19e5f24baa0fed0e4e20cfc35b9283a1d2223a
ltella <lucie.tella@etu.univ-poitiers.fr>
2022-01-17 16:54:00 +0100

    début4

--- app/src/main/java/com/example/presence/etudiantControl.java
+++ app/src/main/java/com/example/presence/etudiantControl.java
@@ -3,6 +3,7 @@ package com.example.presence;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class etudiantControl extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class etudiantControl extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_etudiant_control);
+    } public void finishing(View view) {
+
+        finishAffinity();
+        System.exit(0);
+        // android.os.Process.killProcess(android.os.Process.myPid());
     }
+
 }
