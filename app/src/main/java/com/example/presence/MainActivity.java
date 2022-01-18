5193fef40d4b75f3feee0b7fba78d08b02d8cc4e
ltella <lucie.tella@etu.univ-poitiers.fr>
2022-01-18 11:21:50 +0100

    7

--- app/src/main/java/com/example/presence/MainActivity.java
+++ app/src/main/java/com/example/presence/MainActivity.java
@@ -2,9 +2,17 @@ package com.example.presence;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.app.Activity;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+
+import android.telephony.TelephonyManager;
 import android.util.Log;
+import android.provider.Settings;
+import android.provider.Settings.System;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,10 +20,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String androidID = System.getString(this.getContentResolver(), Settings.Secure.ANDROID_ID);
+        final BluetoothManager manager = (BluetoothManager) getSystemService(BLUETOOTH_SERVICE);
+        manager.getAdapter().getAddress();
+        Log.d("startdeux", androidID);
+        Log.d("startTrois", manager.getAdapter().getAddress());
 
     }
+
     public void start(android.view.View v) {
+
+
         Log.d("start","you are in start");
+
+
+
         Intent lecture = new Intent(this, etudiantControl.class);
 
         startActivity(lecture);
